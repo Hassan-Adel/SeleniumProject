d4a6dcf46ae6b2b9efec69d657bc099632a12350
hassan adel <hasanadel555@hotmail.com>
2018-12-22 23:59:50 +0200

    Add demo

---
+++ SeleniumProject/SeleniumProjectDemo.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject
+{
+    class SeleniumProjectDemo
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void startBrowser()
+        {
+            //driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+        
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
