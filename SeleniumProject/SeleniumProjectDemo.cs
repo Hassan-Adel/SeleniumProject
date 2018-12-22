620123e17237b6c95148b7620949d7141680e314
hassan adel <hasanadel555@hotmail.com>
2018-12-23 01:22:12 +0200

    Attaching Elements

--- SeleniumProject/SeleniumProjectDemo.cs
+++ SeleniumProject/SeleniumProjectDemo.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace SeleniumProject
     class SeleniumProjectDemo
     {
         IWebDriver driver = new ChromeDriver();
+        
 
         [SetUp]
         public void startBrowser()
@@ -23,13 +25,35 @@ namespace SeleniumProject
         [Test]
         public void test()
         {
-            driver.Url = "http://www.google.co.in";
+            Random random = new Random();
+            int randomData = random.Next(10000);
+            string address = string.Format("qa{0:0000}@test.com", randomData);
+            driver.Url = "http://demo.guru99.com/test/newtours/register.php";
+            //Attach Elements
+            IWebElement elementFirstname = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input"));
+            IWebElement elementLastname = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[3]/td[2]/input"));
+            IWebElement elementPhone = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td[2]/input"));
+            IWebElement elementEmail = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[5]/td[2]/input[@id='userName']"));
+            IWebElement elementAddress = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[7]/td[2]/input"));
+            IWebElement elementCity = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[8]/td[2]/input"));
+            IWebElement elementState = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/input"));
+            IWebElement elementPostalCode = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[10]/td[2]/input"));
+            IWebElement elementCountry = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[11]/td[2]/select"));
+            //User info.
+            IWebElement elementUsername = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[13]/td[2]/input[@id='email']"));
+            IWebElement elementPassword = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[14]/td[2]/input"));
+            IWebElement elementConfirmPassword = driver.FindElement(By.XPath("./html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[15]/td[2]/input"));
+
+
+
         }
         
         [TearDown]
         public void closeBrowser()
         {
+            System.Threading.Thread.Sleep(1000);
             driver.Close();
+            driver.Dispose();
         }
 
     }
